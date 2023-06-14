bf9c71057033a3f71ee159ded337143f8097c989
sanago <sanago3130@gmail.com>
2023-06-14 18:19:53 +0200

    UPDATE : New color

--- include/my.h
+++ include/my.h
@@ -26,6 +26,7 @@
     /* ANSI COLORS */
     #define ANSI_COLOR_GREEN "\x1b[32m"
     #define ANSI_COLOR_RED   "\x1b[31m"
+    #define ANSI_COLOR_MAGENTA "\x1b[35m"
     #define ANSI_COLOR_RESET "\x1b[0m"
 
     /* Source functions */
